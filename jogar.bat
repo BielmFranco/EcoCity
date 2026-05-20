7ea1da7d4d960887cafab44ef5c28da3528df8ed
BielmFranco <205702794+BielmFranco@users.noreply.github.com>
2026-05-20 12:38:28 -0300

    Corrige jogar.bat com caminho do Java 21 e encoding UTF-8

--- jogar.bat
+++ jogar.bat
@@ -1,5 +1,4 @@
 @echo off
-chcp 65001 >nul
-echo Iniciando A Ultima Semente...
-java -jar UltimaSemente.jar
+chcp 65001 > nul
+"C:\Program Files\Microsoft\jdk-21.0.11.10-hotspot\bin\java.exe" -Dfile.encoding=UTF-8 -Dstdout.encoding=UTF-8 -jar "%~dp0UltimaSemente.jar"
 pause
